5daa58db8f1f37f97a0b496ecb83bc79a7ab436a
Gustavo Effting <efftinggustavo94@gmail.com>
2024-03-21 17:40:08 -0300

    improve design

---
+++ components/PostItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-white shadow-lg p-4">
+    <h2 class="text-xl font-bold">{{ props.post.title }}</h2>
+    <div v-html="props.post.content"></div>
+  </div>
+</template>
+<script setup lang="ts">
+import type { Post } from '~/types';
+const props = defineProps({
+  post: {
+    type: Object as () => Post,
+    required: true,
+  },
+});
+console.log(props.post.title);
+
+</script>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="h-screen flex justify-center items-center">
-    <div>
-      <h1 class="text-2xl">Display The Paginated Posts Here</h1>
-      <ul class="list-disc list-inside ml-10">
-        <li>Keep performance in mind</li>
-        <li>Make sure to display optimized images</li>
-        <li>Paginate according to your desired strategy</li>
-        <li>Provide a sort order control</li>
-        <li>Store the sort order in the URL</li>
-        <li>Make it look good 💪</li>
-        <li>
-          Then
-          <NuxtLink class="text-blue-500 underline" to="/posts/hello">
-            go to the next task (displaying the individual post)</NuxtLink
-          >
-        </li>
-      </ul>
+  <div class="h-screen flex justify-center p-10 m-auto">
+    <div class="text-center mx-auto max-w-screen-xl">
+      <h1 class="text-xl">Our blog!</h1>
+      <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, esse hic aut error blanditiis
+        explicabo alias optio modi illum veritatis tempora facere doloribus vero qui, laboriosam itaque doloremque,
+        ipsam tenetur!</p>
+      <div class="flex flex-col flex-wrap lg:flex-row">
+        <div v-for="post in allPosts" class="flex-2 lg:w-1/2 p-4">
+          <PostItem :post="post" :key="post.id" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
+<script setup lang="ts">
+import type { Post } from '~/types';
+const { data: posts } = await useFetch("/api/posts", {
+  query: {
+    limit: 20,
+    offset: 0,
+    order: 'oldestFirst',
+  }
+});
+const allPosts = JSON.parse(JSON.stringify(posts.value)) as Post[]; // TODO: use ZOD to validate data and avoid type assertion
+const PostItem = defineAsyncComponent(() => import("~/components/PostItem.vue"));
+</script>
