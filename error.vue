1db85a9993f38868ce5e41ea597221d78152da70
Gustavo Effting <efftinggustavo94@gmail.com>
2024-03-23 18:28:59 -0300

    error not found page

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout>
+    <div class="w-full h-screen flex justify-center items-center p-10">
+      <div class="text-center">
+        <p class="text-5xl">Sorry, Page Not Found</p>
+        <NuxtLink to="/posts">
+          <button class="p-3 bg-blue-600 rounded text-white mt-10">Go back home</button>
+        </NuxtLink>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
